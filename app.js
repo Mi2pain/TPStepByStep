8068f9bc020061017b242d19371f462f8cdec9e9
Mi2pain <hug93@hotmail.fr>
2016-12-06 14:15:45 +0100

    Validation 2

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ console.log("It Works !");
 
 var express = require("express");
 var app = express();
-
-
+var defaultRoute = require("./app/routes/default.route.js");
 var http = require("http");
 var CONFIG = require("./config.json");
+var server = http.createServer(app);
+var path = require("path");
+
 process.env.CONFIG = JSON.stringify(CONFIG);
 
 // init server
-var server = http.createServer(app);
 server.listen(CONFIG.port);
+app.use(defaultRoute);
+app.use("/admin", express.static(path.join(__dirname, "public/admin")));
+app.use("/watch", express.static(path.join(__dirname, "public/watch")));
