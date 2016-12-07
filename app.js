01242b035b0e7761ceea7e9adca1d0cfcc55610b
p1101922 <franzosich@hotmail.fr>
2016-12-07 08:13:50 +0100

    Services /loadPres & /savePres done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-console.log("It Works !");
+console.log("It Works!");
 
 var express = require("express");
 var path = require("path");
-
 var http = require("http");
 var CONFIG = require("./config.json");
+var qs = require('querystring');
+var fs = require('fs');
 
 process.env.CONFIG = JSON.stringify(CONFIG);
 
@@ -17,8 +18,6 @@ var listPres = [];
 
 app.get("/loadPres", function (request, response) {
 
-	fs = require('fs');
-
 	fs.readdir(CONFIG.presentationDirectory, (err, files) => {
 		var index = 0;
 		var extFile = files.filter((file) => path.extname(file) === '.json');
@@ -30,7 +29,6 @@ app.get("/loadPres", function (request, response) {
 				}
 
 				listPres.push(JSON.parse(data.toString()));
-				console.log(JSON.parse(data.toString()));
 			});
 
 			if (++index === extFile.length) {
@@ -40,11 +38,29 @@ app.get("/loadPres", function (request, response) {
 	});
 });
 
-app.get("/savePres", function(request, response) {
-	response.send("It works !");
+app.post("/savePres", function(request, response) {
+	
+	var body = '';
+
+	request.on('data', function (data) {
+		body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+            	request.connection.destroy();
+        });
+
+	request.on('end', function () {
+		var post = JSON.parse(body);
+
+		console.log(JSON.stringify(post));
+            // use post['blah'], etc.
+            fs.writeFile(CONFIG.presentationDirectory + '/' + post.id + ".pres.json", JSON.stringify(post));
+            response.send("Saved!");
+        });
 });
 
-// init server
 app.use(defaultRoute);
 app.use("/admin", express.static(path.join(__dirname, "public/admin")));
 app.use("/watch", express.static(path.join(__dirname, "public/watch")));
